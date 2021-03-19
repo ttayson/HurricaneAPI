4629ba7bbd4762c0f41abd597f29b0783373cb1d
Talles Tayson Saldanha de Farias <talles@caern.local>
2021-03-19 17:04:59 -0300

    route list

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const api = require("./routes/api");
 
 const app = express();
+
+//body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use("/api", api);
 
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const axios = require("axios");
 const cheerio = require("cheerio");
+
+var axios = require("axios");
+var FormData = require("form-data");
 
 const router = express.Router();
 
@@ -12,36 +14,107 @@ router.get("/update", (req, res) => {
   res.json({ info: "teste" });
 });
 
-router.get("/list", (req, res) => {
-  const options = {
-    method: "POST",
+router.post("/list", (req, res) => {
+  var data = new FormData();
+  data.append("email", req.body.login);
+  data.append("pass", req.body.pass);
+
+  var config = {
+    method: "post",
     url: "https://dns.he.net/",
     headers: {
-      cookie:
-        "291541a0a47538f8ae75fdef92aca43c56e2b307832d37725210bb8ed950c8e4=ccec6c6db79ee23780956deb499672f6",
-      "Content-Type": "application/x-www-form-urlencoded",
+      ...data.getHeaders(),
     },
-    data: { email: "ttayson", pass: "Ffarias" },
+    data: data,
   };
 
-  axios
-    .request(options)
+  axios(config)
     .then(function (response) {
-      // console.log(response.data);
       const $ = cheerio.load(response.data);
-      const image = $("img");
-      // value = $("img.Tips").attr("alt");
+      value = $("img.Tips").attr({
+        alt: "delete",
+      });
 
-      value = $(".Tips").attr("alt", "delete");
-      console.log(value);
-      // $("img.Tips").each((i, elem) => {
-      //   {
-      //     console.log(elem);
-      //   }
-      // });
+      var date = [];
+      value.each((i, elem) => {
+        if (
+          value[i].attribs.name != undefined &&
+          value[i].attribs.value != undefined
+        ) {
+          date.push({
+            domain: value[i].attribs.name,
+            id: value[i].attribs.value,
+          });
+        }
+      });
+      if (
+        !req.body.domain ||
+        typeof req.body.domain == undefined ||
+        req.body.domain == null
+      ) {
+        res.json(date);
+      } else {
+        var data = new FormData();
+        data.append("email", req.body.login);
+        data.append("pass", req.body.pass);
+
+        var config = {
+          method: "get",
+          url:
+            "https://dns.he.net/index.cgi?hosted_dns_zoneid=934688&menu=edit_zone&hosted_dns_editzone",
+          headers: {
+            Cookie:
+              "291541a0a47538f8ae75fdef92aca43c56e2b307832d37725210bb8ed950c8e4=1aa2dd6b50e06730c6a9a737c3458aeb",
+            ...data.getHeaders(),
+          },
+          data: data,
+        };
+
+        axios(config).then(function (response) {
+          const $ = cheerio.load(response.data);
+
+          var data = [];
+          value3 = $("td").attr({
+            align: "left",
+          });
+
+          value3.each((i, elem) => {
+            try {
+              if (value3[i].children[0].data != undefined) {
+                data.push(value3[i].children[0].data);
+              } else {
+                data.push(value3[i].children[0].children[0].data);
+              }
+            } catch (error) {}
+          });
+
+          list = [];
+
+          length = data.length;
+          temp = length / 8;
+
+          let i = 0;
+          do {
+            i++;
+            list.push({
+              name: data[2],
+              type: data[3],
+              ttl: data[4],
+              priority: data[5],
+              data: data[6],
+              id: data[1],
+            });
+            console.log(i);
+
+            data.splice(0, 8);
+          } while (i < temp);
+
+          res.json(list);
+        });
+      }
     })
     .catch(function (error) {
-      console.error(error);
+      console.log(error);
     });
 });
 
